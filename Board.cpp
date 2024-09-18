0c04c0433d9dc437cff475f887e781cfa17dfa78
Zoe-Inakassa <zoecoudray@gmail.com>
2024-09-18 17:33:38 -0400

    Début de initBoard et getters

--- Board.cpp
+++ Board.cpp
@@ -1,1 +1,24 @@
 ﻿#include "Board.h"
+
+Board::Board(): mapnoeuds{}, goals{}
+{
+    
+}
+
+void Board::initBoard(const SInitData& _initData)
+{
+    // Enregistrer les tiles pour les trouver rapidement
+    for (auto* pTile = _initData.tileInfoArray; pTile != _initData.tileInfoArray + _initData.tileInfoArraySize; ++pTile) {
+        // Enregistrer hash -> tile
+        int hash = Point::calculerHash(pTile->q, pTile->r);
+        mapnoeuds.insert(std::pair<int, Noeud>(hash, Noeud(*pTile)));
+
+        // Enregistrer les goals
+        if (pTile->type == EHexCellType::Goal) {
+            goals.push_back(&mapnoeuds[hash]);
+        }
+    }
+
+    // Enregistrer les voisins
+    // A CODER
+}
--- Board.h
+++ Board.h
@@ -2,11 +2,26 @@
 #define BOARD_H
 #include <map>
 
+#include "InitData.h"
 #include "Noeud.h"
 
 class Board{
-    std::map<int, Noeud*> mapnoeuds;
+    std::map<int, Noeud> mapnoeuds;
     std::vector<Noeud*> goals;
+    
+public:
+    Board();
+    void initBoard(const SInitData &_initData);
+
+    Noeud *getNoeud(int id)
+    {
+        return &mapnoeuds[id];
+    }
+    
+    const std::vector<Noeud*> &getGoals()
+    {
+        return goals;
+    }
 };
 
 #endif // BOARD_H
