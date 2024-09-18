f0716547f907c384f77115a17bf26edc5343b692
Zoe-Inakassa <zoecoudray@gmail.com>
2024-09-18 13:17:41 -0400

    gitignore ajout de replay et build

--- PlayLocalMatch.bat
+++ PlayLocalMatch.bat
@@ -10,9 +10,9 @@ rem set AIBOOTCAMP2_ENGINE_DIR=D:\AIBootcamp2\AIBootcamp2\Releases
 rem set AIBOT_PATH=D:\AIBootcamp2\LesOutardes\build-solution-x64\Debug\lesoutardes.dll
 rem set TEAM=LesOutardes
 
-set AIBOOTCAMP2_ENGINE_DIR=
-set AIBOT_PATH=
-set TEAM=
+set AIBOOTCAMP2_ENGINE_DIR="..\AIBootCamp2-2024.2.01"
+set AIBOT_PATH=".\build-solution-x64\Debug\myfirstaibot.dll"
+set TEAM="Les Architectes"
 
 rem in s, -1 = default
 set ConnectToProcessDelay=-1
