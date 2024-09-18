477df9b606ebe2de426a5dba7ff8f6d18595f34a
Zoe-Inakassa <zoecoudray@gmail.com>
2024-09-18 15:35:35 -0400

    Ajout de la classe noeud vide et de la struct Point déjà complete

---
+++ Noeud.cpp
@@ -0,0 +1,14 @@
+﻿#include "Noeud.cpp"
+
+
+float Point::calcul_Distance(const Point &destination) const {
+    return (abs(destination.q - q) 
+          + abs(destination.q + destination.r - q - r)
+          + abs(destination.r - r)) / 2;
+}
+
+float Point::calcul_Distance_Coordonnes(int q1, int r1, int q2, int r2) {
+    Point p1 = Point(q1, r1);
+    Point p2 = Point(q2, r2);
+    return p1.calcul_Distance(p2);
+}
---
+++ Noeud.h
@@ -0,0 +1,23 @@
+﻿struct Point {
+    int q;
+    int r;
+
+    Point() = default;
+
+    Point (const int q, const int r) : q(q), r(r) {}
+    float calcul_Distance(const Point& destination) const;
+
+    bool operator==(const Point& other) const {
+        return q == other.q && r == other.r;
+    }
+
+    bool operator!=(const Point &other) const {
+        return !(*this == other);
+    }
+    static float calcul_Distance_Coordonnes(int q1, int r1, int q2, int r2);
+};
+
+public class Noeud
+{
+    
+}
