3b20485eb489f0f87318e260c25238c3683c8d35
Zoe-Inakassa <zoecoudray@gmail.com>
2024-09-18 16:42:48 -0400

    Ajout des attributs de Board

--- Board.h
+++ Board.h
@@ -1,8 +1,12 @@
 ﻿#ifndef BOARD_H
 #define BOARD_H
+#include <map>
+
+#include "Noeud.h"
 
 class Board{
-
+    std::map<int, Noeud*> mapnoeuds;
+    std::vector<Noeud*> goals;
 };
 
 #endif // BOARD_H
