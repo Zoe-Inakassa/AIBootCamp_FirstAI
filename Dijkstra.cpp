01c839033bcfb53c09b41621a4612e2f4c531fc6
Zoe-Inakassa <zoecoudray@gmail.com>
2024-09-19 17:04:36 -0400

    Ajout de la classe Dijkstra avec les fichiers associés

---
+++ Dijkstra.cpp
@@ -0,0 +1,7 @@
+﻿#include "Dijkstra.h"
+
+std::map<const Noeud*, int> Dijkstra::calculerChemin(const Noeud* depart)
+{
+    // A CODER
+    return {};
+}
---
+++ Dijkstra.h
@@ -0,0 +1,13 @@
+﻿#ifndef DIJKSTRA_H
+#define DIJKSTRA_H
+#include <map>
+
+#include "Noeud.h"
+
+
+class Dijkstra{
+public:
+    static std::map<const Noeud*, int> calculerChemin(const Noeud* depart);
+};
+
+#endif
